db07b5e180cd75211c68d25bf74cc3dd797703be
niels-2005 <niels.scholz01@web.de>
2023-06-10 10:54:43 +0200

    added register

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -4,12 +4,31 @@ from django.conf import settings
 
 # Create your models here.
 
+
+# Chat Model
 class Chat(models.Model):
     created_at = models.DateField(default=date.today)
 
+
+# Message Model
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True) 
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE,
+        related_name="chat_message_set",
+        default=None,
+        blank=True,
+        null=True,
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="author_message_set",
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="receiver_message_set",
+    )
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,28 +1,53 @@
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Message, Chat
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
 
-@login_required(login_url='/login/')
+@login_required(login_url="/login/")
 def index(request):
-        if request.method == 'POST':
-             print("Received data " + request.POST['textmessage'])
-             myChat = Chat.objects.get(id=1)
-             Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-        chatMessages = Message.objects.filter(chat__id=1)
-        return render(request, 'chat/index.html', {'messages': chatMessages})
+    if request.method == "POST":
+        print("Received data " + request.POST["textmessage"])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST["textmessage"],
+            chat=myChat,
+            author=request.user,
+            receiver=request.user,
+        )
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, "chat/index.html", {"messages": chatMessages})
 
 
 def login_view(request):
-        redirect = request.GET.get('next')
-        if request.method == 'POST':
-               user = authenticate(username=request.POST.get('username'),  password=request.POST.get('password'))
-               if user:
-                      login(request, user)
-                      return HttpResponseRedirect(request.POST.get('redirect'))
-               else:
-                      return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
-                    
-        return render(request, 'auth/login.html', {'redirect': redirect})
+    redirect = request.GET.get("next")
+    if request.method == "POST":
+        user = authenticate(
+            username=request.POST.get("username"), password=request.POST.get("password")
+        )
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get("redirect"))
+        else:
+            return render(
+                request,
+                "auth/login.html",
+                {"wrongPassword": True, "redirect": redirect},
+            )
+
+    return render(request, "auth/login.html", {"redirect": redirect})
+
+
+# Create your views here.
+def register_view(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/chat/")
+    else:
+        form = RegisterForm()
+
+    return render(response, "auth/register.html", {"form": form})
